d728fcb8350dc3e61418122df2b773a94a89fe14
Mahdi A <81549809+Mahdi-Alizadeh96@users.noreply.github.com>
2021-08-07 00:56:34 +0430

    Update styles and main menu

--- static/blog/js/script.js
+++ static/blog/js/script.js
@@ -4,6 +4,33 @@ $(document).ready(function () {
 });
 // Tooltip
 
+// menu 
+
+const menuBox = document.querySelector('.nav-bar ul');
+
+const colexit = document.querySelector('.collpase-menu i');
+
+const backBlack = document.querySelector('.blackBack');
+
+
+
+colexit.addEventListener('click', function() {
+  if(this.classList.contains('fa-bars')) {
+    this.classList = 'fas fa-times';
+    menuBox.style.right = 0;
+    backBlack.style.display = 'block';
+  }else {
+    this.classList = 'fas fa-bars';
+    menuBox.style.right = '-160px';
+    backBlack.style.display = 'none';
+  }
+})
+
+backBlack.addEventListener('click', () => {
+  colexit.classList = 'fas fa-bars';
+  menuBox.style.right = '-160px';
+  backBlack.style.display = 'none';
+})
 
 
 
